3d2ec64d26db71cde31f84cad7810363a669acbb
Guilherme Augusto Raduenz <graduenz@gmail.com>
2021-09-28 22:44:37 -0300

    some improvements

--- pages/index.js
+++ pages/index.js
@@ -3,6 +3,10 @@ import Image from 'next/image'
 
 import 'flag-icon-css/css/flag-icon.min.css'
 
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faFileDownload } from '@fortawesome/free-solid-svg-icons'
+import { faGithub, faLinkedin } from '@fortawesome/free-brands-svg-icons'
+
 export default function Home() {
   return (
     <main className="mx-auto flex flex-col h-screen items-center">
@@ -11,7 +15,7 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <div className="mt-20 flex flex-col items-center">
+      <div className="mt-20 flex flex-col items-center px-8">
         <Image
           src="/profile.png"
           alt="ugly dev"
@@ -26,29 +30,44 @@ export default function Home() {
         </p>
       </div>
 
-      <div className="mt-10 flex flex-col items-center">
+      <div className="mt-10 flex flex-col items-center px-8">
         <p className="font-medium">
-          I am a .NET Engineer, but you can know everything about me in the links above:
+          I am a .NET Engineer and you will know anything else in the links below:
         </p>
         <div className="mt-10 flex flex-col space-y-2">
           <a
             href="javascript:alert('coming soon')"
-            className="shadow rounded text-center text-sm font-medium px-6 py-3">
-            Want my resume?
+            className="w-40 shadow rounded text-sm font-medium px-6 py-3 flex flex-row">
+            <FontAwesomeIcon
+              icon={faFileDownload}
+              height={16}
+              className="mr-4" />
+            Resume
           </a>
           <a
             href="https://github.com/graduenz"
-            className="shadow rounded text-center text-sm font-medium px-6 py-3">
+            className="shadow rounded text-sm font-medium px-6 py-3 flex flex-row">
+            <FontAwesomeIcon
+              icon={faGithub}
+              height={16}
+              className="mr-4" />
             GitHub
           </a>
           <a
             href="https://linkedin.com/in/graduenz"
-            className="shadow rounded text-center text-sm font-medium px-6 py-3">
+            className="shadow rounded text-sm font-medium px-6 py-3 flex flex-row">
+            <FontAwesomeIcon
+              icon={faLinkedin}
+              height={16}
+              className="mr-4" />
             LinkedIn
           </a>
         </div>
         <p className="font-medium mt-10">
           Please only contact me via LinkedIn.
+        </p>
+        <p className="font-medium mt-4 mb-4">
+          Thanks for visiting!
         </p>
         <span className="flag-icon flag-icon-br mt-4"></span>
       </div>
