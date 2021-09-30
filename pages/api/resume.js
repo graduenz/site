b6d6da1a493519e2eeaf3fc54efd468dee18718d
Guilherme Augusto Raduenz <graduenz@gmail.com>
2021-09-29 21:03:31 -0300

    added resume

--- pages/api/resume.js
+++ pages/api/resume.js
@@ -1,3 +1,9 @@
+const fs = require('fs');
+
 export default (req, res) => {
-    res.status(501).json({ warning: 'Resume is coming soon' })
+    res.setHeader('Content-disposition', 'inline; filename=Guilherme.pdf')
+    res.setHeader('Content-type', 'application/pdf')
+
+    var file = fs.createReadStream('public/Guilherme.pdf')
+    file.pipe(res)
   }
--- pages/index.js
+++ pages/index.js
@@ -37,7 +37,7 @@ export default function Home() {
         <div
           className="mt-10 flex flex-col space-y-2">
           <a
-            href="javascript:alert('coming soon')"
+            href="/api/resume"
             className="shadow rounded text-sm font-medium px-6 py-3 flex flex-row">
             <FontAwesomeIcon
               icon={faFileDownload}
